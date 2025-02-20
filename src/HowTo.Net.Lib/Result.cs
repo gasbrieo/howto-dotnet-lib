1ba533a66e2a03c445513032a4b9c30b36f7450e
Gabriel <gaberabreu.tech@gmail.com>
2025-02-19 22:57:13 -0300

    Initial HowTo.Net.Lib

---
+++ src/HowTo.Net.Lib/Result.cs
@@ -0,0 +1,36 @@
+﻿namespace HowTo.Net.Lib;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public T? Value { get; }
+    public string? Error { get; }
+
+    private Result(T value)
+    {
+        IsSuccess = true;
+        Value = value;
+        Error = null;
+    }
+
+    private Result(string error)
+    {
+        IsSuccess = false;
+        Value = default;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value)
+    {
+        return new(value);
+    }
+
+    public static Result<T> Failure(string error)
+    {
+        if (string.IsNullOrWhiteSpace(error))
+            throw new ArgumentException("Error message cannot be empty.", nameof(error));
+
+        return new Result<T>(error);
+    }
+}
